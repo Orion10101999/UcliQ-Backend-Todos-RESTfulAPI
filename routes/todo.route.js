4a7459c2d66f9833c8f31674767a4e12fe5530be
Sandeep Kumar <orion10101999@gmail.com>
2024-07-03 15:16:53 +0530

    ok

--- routes/todo.route.js
+++ routes/todo.route.js
@@ -12,23 +12,3 @@ router.delete('/:id', verifyToken,todoController.deleteTodo);
 
 module.exports = router;
 
-
-/*
-
-const express = require('express');
-const router = express.Router();
-const todoController = require('../controllers/todoController');
-const authenticate = require('../middlewares/authenticate');
-
-router.use(authenticate);
-
-router.post('/', todoController.createTodo);
-router.get('/', todoController.getTodos);
-router.get('/:id', todoController.getTodoById);
-router.put('/:id', todoController.updateTodo);
-router.delete('/:id', todoController.deleteTodo);
-
-module.exports = router;
-
-
-*/
